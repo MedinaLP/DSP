12731813b648e7e07a7c4cd4909eb13fffffe419
MedinaLP <medinalp@students.national-u.edu.ph>
2025-06-12 02:05:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="AudioVive", page_icon="🎵", layout="centered")
 
-st.header("🎵 AudioVive")
+st.header("🎵 AudioVive 🎵")
 st.subheader("Where old audio finds new life")
