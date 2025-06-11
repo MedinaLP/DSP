cb1cefa0e3e6a97986746a8661a1839f75f618d5
MedinaLP <medinalp@students.national-u.edu.ph>
2025-06-12 02:54:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ st.markdown(
         background-color: ##f0fffe;
     }
     </style>
-    <div style="text-align: center;">
-        <img src="audiovive.png" width="100">
-    </div>
     """,
     unsafe_allow_html=True
 )
 
 st.markdown(
     """
+    <div style="text-align: center;">
+        <img src="audiovive.png" width="100">
+    </div>
     <h1 style='text-align: center; color: #1a1a1a;'>AudioVive</h1>
     <h3 style='text-align: center; font-weight: 400; color: #333;'>Where old audio finds new life</h3>
     """,
